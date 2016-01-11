d01217176334e860f2e7ea2f3a1135e2286f3fb0
stkane <stephenw.kane@gmail.com>
2016-01-11 11:18:20 -0500

    added devise gem, have not yet generated user model

--- app/controllers/statement_of_works_controller.rb
+++ app/controllers/statement_of_works_controller.rb
@@ -1,2 +1,13 @@
 class StatementOfWorksController < ApplicationController
+	def create
+		@contract = Contract.find(params[:contract_id])
+		@statement_of_work = @contract.statement_of_works.create(statement_of_work_params)
+		redirect_to contract_path(@contract)
+	end
+
+	private
+		def statement_of_work_params
+			params.require(:statement_of_work).permit(:description, :product1, :product2, :product3)
+		end
+
 end
