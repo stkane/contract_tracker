39b7e505394fe20b20230a82b58fc6f5a2fb6774
stkane <stephenw.kane@gmail.com>
2016-01-11 20:44:46 -0500

    Adds user model, devise views, and runs db migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   resources :contracts do
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
